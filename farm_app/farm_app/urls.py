4b6e26962a9f4437f5afa6fa7ebc6d32b3cf72dd
Sadi Ahmedova <sadi123@abv.bg>
2023-11-11 16:30:19 +0200

    Update profile picture

--- farm_app/farm_app/urls.py
+++ farm_app/farm_app/urls.py
@@ -12,8 +12,9 @@ urlpatterns = [
                   path('', include('farm_app.catalog.urls')),
                   path('cart/', include('farm_app.cart.urls')),
                   path('api/', include('rest_framework.urls')),
-              ]
-if DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'farm_app.accounts.views.error_404_view'
