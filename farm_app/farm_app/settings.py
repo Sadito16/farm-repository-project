9038d321ae2fdc4b5d7d53616bbfbd48e0b3ac92
Sadi Ahmedova <sadi123@abv.bg>
2024-04-17 12:20:02 +0300

    "Reverting media files and .env in gitignore"# :wq

--- farm_app/farm_app/settings.py
+++ farm_app/farm_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ' ').split(' ')
 
