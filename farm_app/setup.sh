c5d111f5208fd3c01ef0761ef2741ee5ddd47bc0
Sadi Ahmedova <sadi123@abv.bg>
2024-08-24 18:22:28 +0300

    Configure the .env files

---
+++ farm_app/setup.sh
@@ -0,0 +1,9 @@
+
+if [ -f .env ]; then
+	  echo ".env file already exists. Please edit it manually."
+  else
+	      cp .env.example .env
+
+		  echo ".env file created. Please edit it with your configuration values."
+fi
+
